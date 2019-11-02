ae30bfde1541a04cf6e5c24e4b929a5cda073166
github username <github email address>
2019-11-02 11:47:22 -0500

    corgi time

---
+++ src/components/corgiphoto.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Component from 'react';
+
+class Corgi extends Component{
+    render(){
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
